5a1cd2bbe0b22af32e7ccb4dd7bbe9ed8f94b754
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-04-21 22:20:20 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -23,7 +23,7 @@ class Application
         end 
       end 
       elsif req.path.match(/add/)
-      new_item = 
+      new_item = req.params()
     else
       resp.write "Path Not Found"
     end
