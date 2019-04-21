4b4ebaacdb287f3db47e877bc462be281ee55584
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-04-21 22:22:50 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -27,6 +27,8 @@ class Application
         if @@items.include? new_item
           @@cart << new_item
           resp.write "added #{new_item}"
+        else 
+          resp.write "We don't have that item"
     else
       resp.write "Path Not Found"
     end
